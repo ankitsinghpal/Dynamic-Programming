4148ac47bc31a01e4c3e5f5a19aa8a4d0e837e3d
Ankit Singh Pal <35894301+Mr-Perfectt@users.noreply.github.com>
2019-12-27 20:16:14 +0530

    Create fabonacci.cpp

---
+++ fabonacci.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int fabo(int n)
+{
+    int arr[n+2];
+    arr[0]=0;
+    arr[1]=1;
+    for(int i=2;i<=n;i++)
+        arr[i]=arr[i-1]+arr[i-2];
+
+    return arr[n];
+}
+int main() {
+    cout<<fabo(9);
+}
